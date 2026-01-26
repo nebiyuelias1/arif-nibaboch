83c164751e7e68298968df662e7bde1e6ebef027
Nebiyu Elias <nebiyuelias1@gmail.com>
2026-01-26 22:32:40 +0800

    chore: add telegram mini app js

--- backend/app/javascript/application.js
+++ backend/app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  if (window.Telegram && window.Telegram.WebApp) {
+    const webapp = window.Telegram.WebApp;
+    webapp.ready(); // Tells Telegram the app is ready to be displayed
+
+    // Optional: Expand to full height immediately
+    webapp.expand();
+  } else {
+    console.warn("Telegram WebApp SDK not found.");
+  }
+});
