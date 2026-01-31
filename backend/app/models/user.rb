8a9c7b1d63caea8b953b9ffbf6cdcda1a7506605
Nebiyu Elias <nebiyuelias1@gmail.com>
2026-01-31 17:50:54 +0800

    chore: use string keys instead of symbols

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,16 +9,14 @@ class User < ApplicationRecord
   def self.from_telegram_auth(auth_hash)
     auth = auth_hash
 
-    user = where(telegram_id: auth[:id]).first_or_initialize do |u|
+    user = where(telegram_id: auth["id"]).first_or_initialize do |u|
       u.password = Devise.friendly_token[0, 20]
+      # TODO: Find ways of getting email address
+      user.email = auth["email"].presence || "#{auth["id"]}@telegram.com"
     end
-    user.name = [ auth[:first_name], auth[:last_name] ].join(" ").strip
-    # TODO: Find ways of getting email address
-    if user.email.blank?
-      user.email = auth[:email].presence || "#{auth[:id]}@telegram.com"
-    end
-    user.username = auth[:username]
-    user.telegram_id = auth[:id]
+    user.name = [ auth["first_name"], auth["last_name"] ].join(" ").strip
+    user.username = auth["username"]
+    user.telegram_id = auth["id"]
     user.save!
     user
   end
