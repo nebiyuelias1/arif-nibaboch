d69b9ece3b187d8229e187c3e4fd2688a5b2e910
Nebiyu Elias <nebiyuelias1@gmail.com>
2026-01-31 18:02:15 +0800

    chore: fix user bug

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
     user = where(telegram_id: auth["id"]).first_or_initialize do |u|
       u.password = Devise.friendly_token[0, 20]
       # TODO: Find ways of getting email address
-      user.email = "#{auth["id"]}@telegram.com"
+      u.email = "#{auth["id"]}@telegram.com"
     end
     user.name = [ auth["first_name"], auth["last_name"] ].join(" ").strip
     user.username = auth["username"]
