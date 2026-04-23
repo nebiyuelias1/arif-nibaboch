a9763e633db1f81201c3e5fc7c8b3f4517c29d79
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-04-23 23:56:37 +0800

    chore: fix sign-in/out buttons

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
   def authorize_club_owner!(club, redirect_url: nil)
     unless club.owner == current_user
       redirect_to (redirect_url || book_club_path(club)), alert: "You are not authorized to perform this action."
