169d4fcfd2e49e0ffb40c2de9e473221c4311c99
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-06-14 17:30:10 +0800

    chore: reformat routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
       resources :users
 
       root to: "books#index"
-      
     end
   devise_for :users, controllers: {
     registrations: "users/registrations"
