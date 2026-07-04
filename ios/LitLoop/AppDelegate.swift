e98fcd06f1eed29feec9c248fa0ee0e62fc83bd9
Nebiyu Talefe <netale@Nebiyus-Mac-mini.local>
2026-07-05 02:00:56 +0800

    chore: fix ios path config func

--- ios/LitLoop/AppDelegate.swift
+++ ios/LitLoop/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Hotwire.loadConfiguration(from: [
+        Hotwire.loadPathConfiguration(from: [
             .server(baseURL.appending(path: "configurations/ios_v1.json"))
         ])
         return true
