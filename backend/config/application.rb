2de5f06e6f252976981ae537ec456ca3a2bde340
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-07-08 11:30:06 +0800

    chore: update default app name

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -30,7 +30,7 @@ module BackendWithContainer
     # variables. Used by Action Mailer for generating absolute URLs in emails.
     config.x.domain = ENV["APP_DOMAIN"].presence || "localhost:3000"
     config.x.protocol = ENV["APP_PROTOCOL"].presence || "http"
-    config.x.app_name = ENV["APP_NAME"].presence || "Arif Nibaboch"
+    config.x.app_name = ENV["APP_NAME"].presence || "LitLoop"
     # The domain used for the 'from' address in emails. Default to the domain without port.
     config.x.mail_from_domain = ENV["APP_MAIL_FROM_DOMAIN"].presence || config.x.domain.split(":").first
     
